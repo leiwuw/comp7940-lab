20302a0e6430ad1c3d18eb6cfdce47cbf730c1f5
LEI <leiwy_216@126.com>
2025-02-27 10:56:59 +0800

    change chatbot.py file

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ def main():
     # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS TOKEN']),use_context=True)
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']),use_context=True)
     dispatcher =updater.dispatcher
     # You can set this logging module,
     # so you will know when and why things do not work as expected
